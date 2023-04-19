a250d1c117dcf124e5dbc97736c6f426295a2d67
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-04-19 16:51:34 -0600

    añadir archivos para la seccion "Funciones en TypeScript"

---
+++ learn-typescript/src/04-funciones-typescript/FuncionesTypescript.tsx
@@ -0,0 +1,9 @@
+import { Funciones } from "./01-funciones/Funciones";
+
+export const FuncionesTypescript = () => {
+  return (
+    <>
+      <Funciones />
+    </>
+  );
+};
--- learn-typescript/src/App.tsx
+++ learn-typescript/src/App.tsx
@@ -1,22 +1,10 @@
-import { HolaMundo } from "./02-hola-mundo-typescript/HolaMundo";
-import { AnotacionesDeTipo } from "./03-tiposypescript/01-AnotacionesDeTipo/AnotacionesDeTipo";
-import InferenciaDeTipo from "./03-tiposypescript/02-InferenciaDeTipo/InferenciaDeTipo";
-import { TipoDeNumero } from "./03-tiposypescript/03-TipoDeNumero/TipoDeNumero";
-import { TipoDeString } from "./03-tiposypescript/03-TipoDeString/TipoDeString";
-import { TipoDeBooleano } from "./03-tiposypescript/04-TipoDeBooleano/TipoDeBooleano";
-import { TipoDeObjeto } from "./03-tiposypescript/05-TipoDeObjeto/TipoDeObjeto";
-import { TipoDeArray } from "./03-tiposypescript/06-TipoDeArray/TipoDeArray";
-import { TipoDeTuple } from "./03-tiposypescript/07-TipoDeTuple/TipoDeTuple";
-import { TipoDeEnumeracion } from "./03-tiposypescript/08-TipoDeEnumeracion/TipoDeEnumeracion";
-import { TipoAny } from "./03-tiposypescript/09-TipoAny/TipoAny";
-import { TipoNever } from "./03-tiposypescript/10-TipoNever/TipoNever";
-import { TipoUnion } from "./03-tiposypescript/11-TipoUnion/TipoUnion";
-import { AliasDeTipo } from "./03-tiposypescript/12-AliasDeTipo/AliasDeTipo";
+import { FuncionesTypescript } from "./04-funciones-typescript/FuncionesTypescript";
+
 
 function App() {
   return (
     <>
-      <AliasDeTipo />
+      <FuncionesTypescript />
     </>
   );
 }
