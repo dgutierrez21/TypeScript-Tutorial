b400ef320767e52cb50f6a31d4e35e021b407697
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-04-20 10:29:49 -0600

    Corregir nombre de la carpeta "tipo de funciones"

--- learn-typescript/src/04-funciones-typescript/FuncionesTypescript.tsx
+++ learn-typescript/src/04-funciones-typescript/FuncionesTypescript.tsx
@@ -1,5 +1,5 @@
 import { Funciones } from "./01-funciones/Funciones";
-import { TipoDeFunciones } from "./01-tipo-de-funciones/TipoDeFunciones";
+import { TipoDeFunciones } from "./02-tipo-de-funciones/TipoDeFunciones";
 
 export const FuncionesTypescript = () => {
   return (
